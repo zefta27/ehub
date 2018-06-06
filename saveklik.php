c7dfa699cdcf83761d9f53d825651caee73c05aa
zefta27 <zeftaadetya@gmail.com>
2018-06-06 15:24:11 +0700

    Ganti dari laptop lama

--- saveklik.php
+++ saveklik.php
@@ -1,13 +1,19 @@
 <?php
+include_once("classes/Crud.php");
+$crud = new Crud();
 
+$id_histori = rand(0000000,9999999);
 $ipaddress = $_POST['ipaddress'];
 $info =  $_POST['info'];
 $aksi= $_POST['aksi'];
+$waktu = date("Y-m-d");
 
 $data['success'] = true; 
 $data['ipaddress'] = $ipaddress; 
 $data['info'] = $info; 
 $data['aksi'] = $aksi; 
+
+$result = $crud->execute("INSERT INTO klik_histori(id_histori,info,aksi,waktu) VALUES('$id_histori','$ipaddress','$info','$aksi','$waktu')");
 
 echo json_encode($data);
 
